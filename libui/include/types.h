647dbc86895dcfc947ad91e20a1b1f6c1a8f884b
Recep ASLANTAS <m@recp.me>
2015-04-06 00:14:41 +0300

    style

--- libui/include/types.h
+++ libui/include/types.h
@@ -1,9 +1,9 @@
 ﻿/*
-* Copyright (c), Recep Aslantas. All rights reserved.
-*
-* MIT License (MIT), http://opensource.org/licenses/MIT
-* Full license can be found in the LICENSE file
-*/
+ * Copyright (c), Recep Aslantas. All rights reserved.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
 
 #ifndef __libui__types__
 #define __libui__types__
--- libui/src/win/wnd/win-window.cc
+++ libui/src/win/wnd/win-window.cc
@@ -1,9 +1,9 @@
 /*
-* Copyright (c), Recep Aslantas. All rights reserved.
-*
-* MIT License (MIT), http://opensource.org/licenses/MIT
-* Full license can be found in the LICENSE file
-*/
+ * Copyright (c), Recep Aslantas. All rights reserved.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
 
 #include "win-window.h"
 
