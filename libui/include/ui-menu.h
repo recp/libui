8754d13f4533179be7a8f101e2a11a7c2c8db285
Recep ASLANTAS <m@recp.me>
2015-04-10 17:03:08 +0300

    add menu header

---
+++ libui/include/ui-menu.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c), Recep Aslantas. All rights reserved.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef __libui__menu__
+#define __libui__menu__
+
+#include "ui-base.h"
+#include "ui-color.h"
+#include "ui-geometry.h"
+
+namespace ui {
+  
+class _libui_export Menu {
+public:
+   
+};
+
+} // namespace ui
+
+#endif /* defined(__libui__menu__) */
