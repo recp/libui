195248d9fb1a6d23a65796b662d8a147c0657b97
Recep ASLANTAS <m@recp.me>
2015-04-12 14:14:13 +0300

    windows: create simple window

--- libui/src/win/wnd/win-window.cc
+++ libui/src/win/wnd/win-window.cc
@@ -1,34 +1,97 @@
 /*
- * Copyright (c), Recep Aslantas. All rights reserved.
+ * Copyright (c), Recep Aslantas.
  *
  * MIT License (MIT), http://opensource.org/licenses/MIT
  * Full license can be found in the LICENSE file
  */
 
 #include "win-window.h"
+#include <functional>
+
+#include <assert.h>
+#include "../../utils/logutils.h"
+
+#include <stdio.h>
 
 ui::Window::WindowImpl::WindowImpl(Window *_self, Rect rect, int style)
   : m_self(_self) {
 
-  WNDCLASSEX wcex;
+  m_hInstance = GetModuleHandle(NULL);
+
+  const char * classnm = "ede";
+  memcpy(m_szWindowClass, classnm, strlen(classnm));
+
+  WNDCLASSEX wcex = {0};
+ //  memset(&wcex, '\0', sizeof(wcex));
 
   wcex.cbSize = sizeof(WNDCLASSEX);
 
   wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = ::DefWindowProcW;
   // wcex.lpfnWndProc = WndProc;
   wcex.cbClsExtra = 0;
   wcex.cbWndExtra = 0;
   wcex.hInstance = m_hInstance;
-  // wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32PROJECT3));
+  // wcex.hIcon = 
   wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-  // wcex.lpszMenuName = MAKEINTRESOURCE(IDC_WIN32PROJECT3);
-  // wcex.lpszClassName = szWindowClass;
-  // wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-  m_className = RegisterClassEx(&wcex);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+  // wcex.lpszMenuName = 
+  wcex.lpszClassName = m_szWindowClass;
+  // wcex.hIconSm =
   
+  RegisterClassEx(&wcex);
+  
+  m_hWnd = CreateWindow(m_szWindowClass, 
+                        m_szTitle, 
+                        WS_OVERLAPPEDWINDOW,
+                        CW_USEDEFAULT, 
+                        0, 
+                        CW_USEDEFAULT, 
+                        0,
+                        NULL, 
+                        NULL,
+                        m_hInstance, 
+                        NULL);
+
+  if (!m_hWnd) {
+    debug::logt("win:window", "couldnt create window, last err: %d", 
+                GetLastError());
+  }
+
   m_self = _self;
+}
+
+LRESULT CALLBACK 
+ui::Window::WindowImpl::WndProc(HWND hWnd, 
+                                UINT message, 
+                                WPARAM wParam, 
+                                LPARAM lParam) {
+  int wmId, wmEvent;
+  PAINTSTRUCT ps;
+  HDC hdc;
+
+  switch (message) {
+  case WM_COMMAND:
+    wmId = LOWORD(wParam);
+    wmEvent = HIWORD(wParam);
+    switch (wmId) {
+    default:
+      return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    break;
+  case WM_PAINT:
+    hdc = BeginPaint(hWnd, &ps);
+
+    EndPaint(hWnd, &ps);
+    break;
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+  default:
+    return DefWindowProc(hWnd, message, wParam, lParam);
+  }
+
+  return 0;
 }
 
 void ui::Window::WindowImpl::setTitle(const char *title) const {
@@ -36,7 +99,8 @@ void ui::Window::WindowImpl::setTitle(const char *title) const {
 }
 
 void ui::Window::WindowImpl::show() const {
- 
+  ShowWindow(m_hWnd, 1);
+  UpdateWindow(m_hWnd);
 }
 
 ui::View * ui::Window::WindowImpl::contentView() const {
@@ -48,5 +112,5 @@ void ui::Window::WindowImpl::setContentView(ui::View *view) {
 }
 
 ui::Window::WindowImpl::~WindowImpl() {
-  m_wnd = nullptr;
+
 }
