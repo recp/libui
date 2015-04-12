c9293c2a5be4c6291975477aba85d28a8f8d33b0
Recep ASLANTAS <m@recp.me>
2015-04-12 14:08:45 +0300

    license correction

--- libui/src/win/view/win-view.h
+++ libui/src/win/view/win-view.h
@@ -1,9 +1,9 @@
 /*
-* Copyright (c), Recep Aslantas. All rights reserved.
-*
-* MIT License (MIT), http://opensource.org/licenses/MIT
-* Full license can be found in the LICENSE file
-*/
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
 
 #ifndef __libui__win__view__
 #define __libui__win__view__
