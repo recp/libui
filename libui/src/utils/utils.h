591d8aa3f5042177fb8f10300f8a7f3b4b39aef9
Recep ASLANTAS <m@recp.me>
2015-04-12 13:53:50 +0300

    add utils files for general purpose

---
+++ libui/src/utils/utils.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef __libui_win_window__
+#define __libui_win_window__
+
+#include "../config.h"
+
+namespace ui {
+
+} // namespace ui
+
+#endif /* defined(__libui_win_window__) */
