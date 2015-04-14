573c5af1475a10afc945dda26f1c4e80d5c6f0d7
Recep ASLANTAS <m@recp.me>
2015-04-14 22:08:54 +0300

    menu, menuitem

---
+++ libui/include/ui-image.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef __libui__image__
+#define __libui__image__
+
+#include "ui-base.h"
+
+namespace ui {
+
+class _libui_export Image {
+  
+};
+
+} // namespace ui
+
+#endif /* defined(__libui__image__) */
--- libui/include/ui-menu.h
+++ libui/include/ui-menu.h
@@ -12,11 +12,44 @@
 #include "ui-color.h"
 #include "ui-geometry.h"
 
+#include <vector>
+
 namespace ui {
-  
+
+class _libui_export Image;
+class _libui_export MenuItem;
+
 class _libui_export Menu {
 public:
-   
+  Menu();
+  Menu(CStringPtr title);
+
+  CStringPtr title() const;
+  void title(CStringPtr title) const;
+
+  std::vector<MenuItem *> menuItems() const;
+  void addSubview(MenuItem * menuItem) const;
+};
+
+class _libui_export MenuItem {
+public:
+  MenuItem();
+  MenuItem(CStringPtr title);
+
+  Menu * menu() const;
+  Menu * subMenu() const;
+
+  CStringPtr title() const;
+  void title(CStringPtr title) const;
+
+  Image image() const;
+  void image(Image image) const;
+
+  bool highlighted() const;
+  void highlighted(bool highlighted);
+
+  bool enabled() const;
+  void enabled(bool enabled);
 };
 
 } // namespace ui
